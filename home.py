9ce34f60819242e8a3736c41918d298842260bf8
Alessouz <93060660+Alessouz@users.noreply.github.com>
2023-08-08 12:53:46 -0300

    erro.1

--- home.py
+++ home.py
@@ -7,6 +7,10 @@ import os
 from urllib.parse import unquote
 import re
 
+# Carregar os templates usando o Jinja2
+env = Environment(loader=FileSystemLoader('.'))
+template = env.get_template('landing_page.html')
+
 # Função para carregar os dados do arquivo JSON
 def load_data():
     try:
@@ -134,7 +138,6 @@ novos_itens = [
 # Adicione os novos itens à lista existente
 lista_de_itens.extend(novos_itens)
 
-
 url_encoded = "MANTA%20DE%20CASAL%20+%20ALMOFADA.."
 
 # Decodificar a string
@@ -145,7 +148,6 @@ cleaned_text = re.sub(r'[^a-zA-Z0-9\s]', '', decoded_text)
 
 # Imprimir o resultado
 print(cleaned_text)
-
 
 def enviar_email(mensagem, destinatario):
     msg = email.message.Message()
@@ -164,18 +166,15 @@ def enviar_email(mensagem, destinatario):
     print('Email enviado')
     s.quit()
 
-
 def chunk_list(lst, chunk_size):
     """Divide uma lista em pedaços do tamanho especificado."""
     return [lst[i:i + chunk_size] for i in range(0, len(lst), chunk_size)]
-
 
 env = Environment(loader=FileSystemLoader('.'))
 template = env.get_template('/templates/landing_page.html')
 
 itens_por_pagina = len(lista_de_itens) // 3
 lista_dividida = chunk_list(lista_de_itens, itens_por_pagina)
-
 
 @app.route('/')
 def landing_page():
@@ -186,12 +185,9 @@ def landing_page():
     return render_template('landing_page.html', lista_de_itens=lista_dividida[0], itens_escolhidos=itens_escolhidos)
 
 # Rota para servir as imagens estáticas
-
-
 @app.route('/imagens/<path:filename>')
 def servir_imagens(filename):
     return send_from_directory('imagens', filename)
-
 
 @app.route('/pagina2')
 def pagina2():
@@ -199,18 +195,15 @@ def pagina2():
     itens_escolhidos = list(data.values())
     return render_template('pagina2.html', lista_de_itens=lista_dividida[1], itens_escolhidos=itens_escolhidos)
 
-
 @app.route('/pagina3')
 def pagina3():
     data = load_data()
     itens_escolhidos = list(data.values())
     return render_template('pagina3.html', lista_de_itens=lista_dividida[2], itens_escolhidos=itens_escolhidos)
 
-
 @app.route('/item/<item>')
 def item_details(item):
     return render_template('formulario.html', item=item)
-
 
 @app.route('/enviar_email', methods=['POST'])
 def enviar_formulario():
@@ -235,7 +228,6 @@ def enviar_formulario():
         """
 
         # Definir o endereço de e-mail de destino como o seu endereço de e-mail
-        # Insira o e-mail do destinatário aqui
         email_destino = 'filho.pantaleao@gmail.com'
 
         # Passa a mensagem e o endereço de e-mail de destino como argumentos
@@ -247,7 +239,6 @@ def url_decode():
     encoded_url = request.args.get('url')
     decoded_url = unquote(encoded_url)
     return decoded_url
-
 
 if __name__ == '__main__':
     app.run(debug=True)
