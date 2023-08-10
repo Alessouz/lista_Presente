6a4e5b30a23345c8315e49569642a7e424c9de32
Alessouz <93060660+Alessouz@users.noreply.github.com>
2023-08-10 10:21:39 -0300

    quinta parte V.3

--- home.py
+++ home.py
@@ -132,6 +132,24 @@ novos_itens = [
 # Adicione os novos itens à lista existente
 lista_de_itens.extend(novos_itens)
 
+# Sua lista de itens com "%20"
+lista_de_itens = [
+    'CAFETEIRA',
+    'SANDUICHEIRA',
+    'LIQUIDIFICADOR%20VERMELHO',
+    'BATEDEIRA%20PROFISSIONAL',
+    # ... outros itens ...
+]
+
+# Removendo "%20" da lista
+lista_de_itens_sem_espacos = [item.replace('%20', ' ') for item in lista_de_itens]
+
+# Imprimindo a nova lista
+for item in lista_de_itens_sem_espacos:
+    print(item)
+
+
+
 def enviar_email(mensagem, destinatario):
     msg = email.message.Message()
     msg['Subject'] = "Escolheram um item na lista do chá "
